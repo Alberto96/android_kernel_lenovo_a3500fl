45ba79c099dfdb1f8220d040e94fc3452304e11b
Alberto96 <albertoguanti@gmail.com>
2015-09-04 23:51:39 +0200

    mt6582: make scripts much more generic

--- kernel.sh
+++ kernel.sh
@@ -1,7 +1,10 @@
-export PATH=/home/AndroidDev/Lenovo/arm-eabi-4.7/bin:$PATH
+#!/bin/bash
+
+LOCAL_PATH=`pwd -P`
+
+export PATH=/opt/arm-eabi-4.7/bin:$PATH
 export CROSS_COMPILE=arm-eabi-
 export TARGET_PRODUCT=lcsh82_tb_3gfull_kk
-export FLASHABLE_ZIP="./kernel-$TARGET_PRODUCT-`date +"%Y%m%d"`.zip"
 
 ./mk -o=TARGET_BUILD_VARIANT=user $TARGET_PRODUCT n k
-source /home/AndroidDev/Lenovo/alps/pack_bootimage.sh
+source $LOCAL_PATH/pack_bootimage.sh
--- pack_bootimage.sh
+++ pack_bootimage.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+LOCAL_PATH=`pwd -P`
+
 set -e
 
-KERNEL_DIR=/home/AndroidDev/Lenovo/alps
+KERNEL_DIR=$LOCAL_PATH
 export TARGET_PRODUCT=lcsh82_tb_3gfull_kk
-RAMDISK_DIRECTORY=/home/AndroidDev/Lenovo/alps/ramdisk/$TARGET_PRODUCT
+RAMDISK_DIRECTORY=$LOCAL_PATH/ramdisk/$TARGET_PRODUCT
 
 rm -rf $KERNEL_DIR/out/target/product/$TARGET_PRODUCT/kernel
 rm -rf $KERNEL_DIR/out/target/product/$TARGET_PRODUCT/boot.img
 $KERNEL_DIR/mediatek/build/tools/mkimage $KERNEL_DIR/out/target/product/lcsh82_tb_3gfull_kk/obj/KERNEL_OBJ/arch/arm/boot/zImage KERNEL > $KERNEL_DIR/out/target/product/$TARGET_PRODUCT/kernel
 
-/home/AndroidDev/Lenovo/alps/mtk-tools/repack-MTK.pl -boot $KERNEL_DIR/out/target/product/$TARGET_PRODUCT/kernel $RAMDISK_DIRECTORY $KERNEL_DIR/out/target/product/$TARGET_PRODUCT/boot.img
+$LOCAL_PATH/mtk-tools/repack-MTK.pl -boot $KERNEL_DIR/out/target/product/$TARGET_PRODUCT/kernel $RAMDISK_DIRECTORY $KERNEL_DIR/out/target/product/$TARGET_PRODUCT/boot.img
